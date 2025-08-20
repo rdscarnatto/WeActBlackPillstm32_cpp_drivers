becb379f8bff0a6d289ddda7ec2078efcc20f511
rdscarnatto <dscarnatto@gmail.com>
2025-08-20 06:37:16 -0300

    update spi config

--- lib/spi/spi.cpp
+++ lib/spi/spi.cpp
@@ -4,6 +4,36 @@ void SPI_init(SPI_Handle_t *pSPIHandle)
 {
   // Enable SPI clock
   SPI1_PCLK_EN();
-  //
-  // SPI_TypeDef *pSPIx = NULL;
+  
+  // Configure SPI_CR1 register
+  uint32_t tempreg = 0;
+
+  //1. Configure the device mode (MSTR of SPI_CR1)
+  tempreg |= pSPIHandle->SPI_Config.SPI_DeviceMode << 2;
+
+  //2. Configure de BusConfig
+  if (pSPIHandle->SPI_Config.SPI_BusConfig == SPI_BUS_CONFIG_FD)
+  {
+    tempreg &= ~(1 << 15);
+  }else if (pSPIHandle->SPI_Config.SPI_BusConfig == SPI_BUS_CONFIG_HD)
+  {
+    tempreg |= (1 << 15);
+  }else if (pSPIHandle->SPI_Config.SPI_BusConfig == SPI_BUS_CONFIG_S_RXONLY){
+    tempreg &= ~(1 << 15);
+    tempreg |= (1 << 10);
+  }
+  //3. Configure serial clock speed (baudrate)
+  tempreg |= pSPIHandle->SPI_Config.SPI_SclkSpeed << 3; 
+
+  //4. Confiure the DFF
+  tempreg |= pSPIHandle->SPI_Config.SPI_DFF << 11;
+
+  //5. Configure the CPOL
+  tempreg |= pSPIHandle->SPI_Config.SPI_CPOL << 1;
+
+  //6. Configure the CPHA
+  tempreg |= pSPIHandle->SPI_Config.SPI_CPHA << 0;
+
+  pSPIHandle->pSPIx->CR1 |= tempreg;
+
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "uart.hpp"
+#include "spi.hpp"
 
 int main()
 {
 
   uart_init();
+
   /* code */
   return 0;
 }
